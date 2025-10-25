5c3d8dc66f882c21e8d80d4ea19208090e2c41a0
f4mno123 <f4mno123@gmail.com>
2025-10-26 01:41:13 +0200

    Initial Commit

---
+++ Gamebit/GamebitApp.swift
@@ -0,0 +1,17 @@
+//
+//  GamebitApp.swift
+//  Gamebit
+//
+//  Created by Albert Rządziński on 26/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct GamebitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
